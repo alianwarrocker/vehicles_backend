123a8051a5ea83d10a50e01d403aa6c45263f3c8
root <root@localhost.localdomain>
2017-06-23 17:52:44 +0530

    commits

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,10 @@ var URL="mongodb://newone:newone@ds131742.mlab.com:31742/newdatabase";
 
 const db=require('monk')(URL);
 
-const docs=db.get('mongocollect');
+//const docs=db.get('mongocollect');
+const  docs=db.get('secondcollection')
 /* GET home page. */
-router.get('/welcome', function(req, res, next) {
+//router.get('/welcome', function(req, res, next) {
     // res.render('index', { title: 'Express' });
 
   /* docs.find({},function(err,docs){
@@ -15,18 +16,34 @@ router.get('/welcome', function(req, res, next) {
         else res.json(docs[0]);
     })*/
 
-  docs.insert({"name":"Sweta", "age":"21","year":"2017"}, function(err,docs){
-        if(err) console.log(err)
-        else res.send("Finally I am done");
-    })
+  //docs.insert({"name":"Sweta", "age":"21","year":"2017"}, function(err,docs){
+    //    if(err) console.log(err)
+      //  else res.send("Finally I am done");
+    //})
 
 
 
+//});
+
+
+//router.get('/wel', function(req, res, next) {
+ //   res.send('Firdaus');
+//});
+
+/*pushing data to node*/
+router.get('/push',function(req,res,next)
+{
+    docs.update(
+        {
+            "emailid": "alianwar@gmail.com",
+            "id": "53g3",
+            "name": "ali"},
+        {$push: {"group":{"name": "user3"}}
+    },
+        function(err,docs) {
+            if (err) console.log(err)
+            else  res.send("successful");
+        }
+        )
 });
-
-
-router.get('/wel', function(req, res, next) {
-    res.send('Firdaus');
-});
-
 module.exports = router;
